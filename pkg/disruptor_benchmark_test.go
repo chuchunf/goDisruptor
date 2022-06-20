cf9a15d03a57fd933fa7ec45701f1fb69a5831b0
chuchunf <chuchunf@users.noreply.github.com>
2022-06-20 21:24:07 +0800

    add benchmart test for disruptor

--- pkg/disruptor_benchmark_test.go
+++ pkg/disruptor_benchmark_test.go
@@ -1,8 +1,44 @@
 package pkg
 
 import (
+	internal "goDisruptor/internal"
+	"sync"
 	"testing"
 )
 
 func BenchmarkDisruptor(b *testing.B) {
+	size := int64(1024)
+	ringbuffer, _ := internal.NewRingBuffer[int64](size, internal.NewSequencer(size))
+	barrier := ringbuffer.CreateBarrier()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	iterations := int32(b.N)
+	go func() {
+		defer wg.Done()
+		for i := int32(0); i < iterations; i++ {
+			seq := ringbuffer.Next()
+			pooled := ringbuffer.Get(seq)
+			*pooled = int64(i)
+			ringbuffer.Publish(seq)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		seq := internal.NewSequence()
+		ringbuffer.AddGatingSequence(&seq)
+		for i := int32(0); i < iterations; i++ {
+			next := seq.Get() + 1
+			next = barrier.WaitFor(next)
+			ringbuffer.Get(next)
+			seq.Set(next)
+		}
+	}()
+
+	wg.Wait()
 }
