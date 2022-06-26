baaf0d9072c1b86846fe168fa966d06e147a62e8
chuchunf <chuchunf@users.noreply.github.com>
2022-06-26 13:11:11 +0800

    compare gc and nogc benchmark for waitstrategy

--- internal/waitStrategy_benchmark_test.go
+++ internal/waitStrategy_benchmark_test.go
@@ -1,11 +1,24 @@
 package pkg
 
 import (
+	"runtime/debug"
 	"testing"
 )
 
-// stop GC makes this lower ...
 func BenchmarkBusySpin(b *testing.B) {
+	seq := NewSequence()
+	seq.Set(100)
+	strategy := BusySpinWaitStrategy{}
+	for i := 0; i < b.N; i++ {
+		strategy.waitFor(99, &seq)
+	}
+}
+
+// from cpuprofile, the none GC version has simpler call graph and in general fast
+// no GC version should be used for performance tuning
+func BenchmarkBusySpinWithoutGC(b *testing.B) {
+	debug.SetGCPercent(-1)
+
 	seq := NewSequence()
 	seq.Set(100)
 	strategy := BusySpinWaitStrategy{}
