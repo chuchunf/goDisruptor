38878d4ccdd6a89b880ce06597e9bd135c7a581e
chuchunfeng <chuchunf@users.noreply.github.com>
2025-09-04 22:57:07 +0800

    add a benchmark for ring buffer with 2 read and 1 writer with forcing switch CPU

--- internal/ringbuffer_benchmark_test.go
+++ internal/ringbuffer_benchmark_test.go
@@ -140,3 +140,91 @@ func BenchmarkRingBufferPinCPU(b *testing.B) {
 
 	wg.Wait()
 }
+
+func switchCPU(changeCPU bool, cpu1 unix.CPUSet, cpu2 unix.CPUSet) bool {
+	runtime.UnlockOSThread()
+	runtime.LockOSThread()
+	if changeCPU {
+		unix.SchedSetaffinity(0, &cpu1)
+	} else {
+		unix.SchedSetaffinity(0, &cpu2)
+	}
+	return !changeCPU
+}
+
+func BenchmarkRingBufferForceSwitchCPU(b *testing.B) {
+	if runtime.NumCPU() < 8 {
+		panic("need 8 CPU cores for benchmark !")
+	}
+	var cpuset1, cpuset2, cpuset3 = unix.CPUSet{}, unix.CPUSet{}, unix.CPUSet{}
+	cpuset1.Set(1)
+	cpuset2.Set(2)
+	cpuset3.Set(3)
+	var cpuset4, cpuset5, cpuset6 = unix.CPUSet{}, unix.CPUSet{}, unix.CPUSet{}
+	cpuset1.Set(4)
+	cpuset2.Set(5)
+	cpuset3.Set(6)
+
+	iterations := b.N
+	seqcer := NewSequencer(1024)
+
+	seq1 := NewSequence()
+	seq2 := NewSequence()
+
+	seqcer.addGatingSequences(&seq1)
+	seqcer.addGatingSequences(&seq2)
+
+	ring, _ := NewRingBuffer[int64](1024, seqcer)
+
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	reader1 := func() {
+		changeCpu := true
+		runtime.LockOSThread()
+		unix.SchedSetaffinity(0, &cpuset1)
+		defer runtime.UnlockOSThread()
+		defer wg.Done()
+		for i := 0; i < iterations; i++ {
+			if i%100_000 == 0 {
+				changeCpu = switchCPU(changeCpu, cpuset1, cpuset4)
+			}
+			ring.Get(int64(i))
+			seq1.Set(int64(i))
+		}
+	}
+	reader2 := func() {
+		changeCpu := true
+		runtime.LockOSThread()
+		unix.SchedSetaffinity(0, &cpuset2)
+		defer runtime.UnlockOSThread()
+		defer wg.Done()
+		for i := 0; i < iterations; i++ {
+			if i%100_000 == 0 {
+				changeCpu = switchCPU(changeCpu, cpuset2, cpuset5)
+			}
+			ring.Get(int64(i))
+			seq2.Set(int64(i))
+		}
+	}
+	writer := func() {
+		changeCpu := true
+		runtime.LockOSThread()
+		unix.SchedSetaffinity(0, &cpuset3)
+		defer runtime.UnlockOSThread()
+		defer wg.Done()
+		for i := 0; i < iterations; i++ {
+			if i%100_000 == 0 {
+				changeCpu = switchCPU(changeCpu, cpuset3, cpuset6)
+			}
+			ring.Get(int64(i))
+			ring.Publish(int64(i))
+		}
+	}
+
+	go reader1()
+	go reader2()
+	go writer()
+
+	wg.Wait()
+}
